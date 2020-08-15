61be5cb39e4841995760d6ecfd133a81069f7c7d
Antonis Antonopoulos <antony.ant1985@gmail.com>
2020-08-15 19:08:35 +0300

    WD fixes

--- js/resume.js
+++ js/resume.js
@@ -4,7 +4,9 @@
   $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
-    $('body').delay(350).css({'overflow':'visible'});
+    $('body').delay(350).css({
+      'overflow': 'visible'
+    });
   })
 
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -30,10 +32,15 @@
   $('[data-toggle="popover"]').popover();
 })(jQuery);
 
-$.get( 'https://raw.githubusercontent.com/pseudorandomized/pseudorandomized.github.io/master/news.yaml', function( data ) {
-        var obj1 = jsyaml.load( data );
-        console.log(obj1);
-        for (ns in obj1["news"]) {
-          document.getElementById("123").innerHTML += "<p>"+obj1["news"][ns]+"</p>";
-        }
-    });
+$.get('https://raw.githubusercontent.com/pseudorandomized/pseudorandomized.github.io/master/news.yaml', function(data) {
+  var obj1 = jsyaml.load(data);
+
+  for (ns in obj1["news"]) {
+    document.getElementById("123").innerHTML += "<p>" + obj1["news"][ns] + "</p>";
+  }
+});
+
+function showmodal(x) {
+  let y = '<iframe src="' + x + '" width="900" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>';
+  document.getElementById("mod-bod").innerHTML = y;
+}
